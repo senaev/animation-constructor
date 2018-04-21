7ffc2a8788572767630f1828e5c1288e3b128824
Andrey Senaev <senaev@yandex-team.ru>
2018-04-21 23:33:18 +0500

    remove animation workarounds

--- src/Store/utils/createDefaultUnitScript.ts
+++ src/Store/utils/createDefaultUnitScript.ts
@@ -8,50 +8,8 @@ export function createDefaultUnitScript<T extends Unit>(unit: T, value: UnitType
         unit,
         steps: [
             {
-                duration: 0.3,
+                duration: 1,
                 value,
-                easing: Easing.easeInOut,
-            },
-            {
-                duration: 0.3,
-                value: typeof value === 'number'
-                    ? 10
-                    : typeof value === 'boolean'
-                        ? true
-                        : {
-                            r: 255,
-                            g: 0,
-                            b: 0,
-                            a: 1,
-                        },
-                easing: Easing.easeInOut,
-            },
-            {
-                duration: 0.3,
-                value: typeof value === 'number'
-                    ? 30
-                    : typeof value === 'boolean'
-                        ? false
-                        : {
-                            r: 0,
-                            g: 255,
-                            b: 0,
-                            a: 0.2,
-                        },
-                easing: Easing.easeInOut,
-            },
-            {
-                duration: 0.1,
-                value: typeof value === 'number'
-                    ? 10
-                    : typeof value === 'boolean'
-                        ? true
-                        : {
-                            r: 0,
-                            g: 0,
-                            b: 255,
-                            a: 1,
-                        },
                 easing: Easing.easeInOut,
             },
         ],
